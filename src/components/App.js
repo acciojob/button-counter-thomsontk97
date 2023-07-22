73a16d3942c2dc1696a0a1bfe6d9cbc1f5cde07f
Thomson T Kakkanattu <thomsontk237@gmail.com>
2023-07-22 13:31:49 +0000

    Submit-2

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   let [count, setCount] = useState(0);
   return (
     <div>
-      <p>Button clicked {count} times </p>
+      <p>Button clicked {count} times</p>
       <button onClick={() => setCount(count+1)}>Click me</button>
     </div>
   )
